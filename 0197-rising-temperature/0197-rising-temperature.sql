4018a34db43c11dcbaed76f2a672798bc9ee18d9
Sourav Kundu <74766939+SouravKunduSK@users.noreply.github.com>
2023-08-05 13:34:09 +0600

    Time: 1151 ms (32.62%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,3 +1,4 @@
 /* Write your T-SQL query statement below */
 SELECT a.id FROM Weather a, Weather b
 WHERE a.recordDate > b.recordDate AND datediff(DAY,a.recordDate,b.recordDate) = -1 AND  a.temperature > b.temperature 
+
