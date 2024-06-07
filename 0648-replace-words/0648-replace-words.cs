9fa99859293eb5803c65f88d4982cf54237e41d9
Sourav Kundu <74766939+SouravKunduSK@users.noreply.github.com>
2024-06-07 08:52:52 +0600

    Time: 227 ms (44.90%), Space: 64.4 MB (97.96%) - LeetHub

---
+++ 0648-replace-words/0648-replace-words.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public string ReplaceWords(IList<string> dictionary, string sentence) {
+        
+        var roots = dictionary.ToList();
+        var words = sentence.Split(" ");
+        
+        for(int i = 0; i < words.Length; ++i)
+        {
+            var replacement = words[i];
+            
+            foreach(var root in roots)
+            {
+                if((replacement.StartsWith(root)) && (root.Length<replacement.Length))
+                {
+                    replacement = root;
+                }
+            }
+            words[i] = replacement;
+        }
+        
+        return string.Join(" ", words);
+    }
+}
