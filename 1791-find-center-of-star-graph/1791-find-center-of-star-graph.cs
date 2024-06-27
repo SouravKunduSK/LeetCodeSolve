3cb30c74288ae9e379ac4bdb27b1d74e58e32800
Sourav Kundu <74766939+SouravKunduSK@users.noreply.github.com>
2024-06-27 12:04:49 +0600

    Time: 382 ms (73.91%), Space: 69.2 MB (91.30%) - LeetHub

---
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int FindCenter(int[][] edges) {
+    int node1 = edges[0][0];
+    int node2 = edges[0][1];
+    int node3 = edges[1][0];
+    int node4 = edges[1][1];
+    
+    
+    if (node1 == node3 || node1 == node4) {
+        return node1;
+    }
+    
+    return node2;
+    }
+}
