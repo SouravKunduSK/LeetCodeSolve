351e07ead4800201f35f8b5e2d738cc5a9fafb27
Sourav Kundu <74766939+SouravKunduSK@users.noreply.github.com>
2024-07-10 11:56:27 +0600

    Time: 64 ms (32.88%), Space: 40.8 MB (24.66%) - LeetHub

---
+++ 1598-crawler-log-folder/1598-crawler-log-folder.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int MinOperations(string[] logs) {
+        int depth = 0;
+        
+        foreach (var log in logs) {
+            if (log == "../") {
+                if (depth > 0) {
+                    depth--;
+                }
+            } else if (log != "./") {
+                depth++;
+            }
+        }
+        
+        return depth;
+    }
+}
